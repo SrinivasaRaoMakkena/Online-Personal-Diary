92bfcc90536cc80471cec07f62edcf544a925bbe
SrinivasaRaoMakkena <makkenasrinivasarao1@gmail.com>
2017-04-25 16:17:03 -0500

    COmmit with updates in all pages

---
+++ web/Notes.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+window.onload = function() {
+   document.getElementById("submit").onclick = save;
+}
+
+function save(){
+//    if (document.getElementById("noteArea").value = ""){        
+//        alert('Please enter your thoughts in the text area below!')
+//    }else{
+        alert("Your thoughts saved successfully!!")
+    
+}
